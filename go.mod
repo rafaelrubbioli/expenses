acf41d1807adcb6139a97106d1f96478109bdaa8
VictorAssunc <hugotex00@gmail.com>
2020-10-16 12:44:08 -0300

    feat(user): user repository

--- go.mod
+++ go.mod
@@ -5,8 +5,12 @@ go 1.13
 require (
 	github.com/99designs/gqlgen v0.10.1
 	github.com/go-chi/chi v3.3.2+incompatible
-	github.com/jmoiron/sqlx v1.2.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.2.1-0.20191011153232-f91d3411e481 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/vektah/gqlparser v1.1.2
-	github.com/volatiletech/sqlboiler/v4 v4.2.0 // indirect
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
---
+++ pkg/service/internal/repository/user.go
@@ -0,0 +1,61 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/rafaelrubbioli/espenses/pkg/entity"
+	"github.com/rafaelrubbioli/espenses/pkg/lib/db"
+)
+
+type User interface{
+	Create(ctx context.Context, name string) (int64, error)
+	Get(ctx context.Context, id int64) (*entity.User, error)
+	Update(ctx context.Context, user *entity.User) error
+	Delete(ctx context.Context, id int64) error
+}
+
+type user struct {
+	db db.MySQL
+}
+
+func NewUserRepository(db db.MySQL) User {
+	return user{db: db}
+}
+
+func (r user) Create(ctx context.Context, name string) (int64, error) {
+	query := `INSERT INTO users (name) VALUES (?)`
+	result, err := r.db.Exec(ctx, query, name)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, nil
+	}
+
+	return id, nil
+}
+
+func (r user) Get(ctx context.Context, id int64) (*entity.User, error) {
+	var user entity.User
+	query := `SELECT id, name FROM users WHERE id = ? LIMIT 1`
+	err := r.db.Get(ctx, &user, query, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r user) Update(ctx context.Context, user *entity.User) error {
+	query := `UPDATE users SET name = (?) WHERE id = ?`
+	_, err := r.db.Exec(ctx, query, user.Name, user.ID)
+	return err
+}
+
+func (r user) Delete(ctx context.Context, id int64) error {
+	query := `DELETE FROM users WHERE id = ?`
+	_, err := r.db.Exec(ctx, query, id)
+	return err
+}
